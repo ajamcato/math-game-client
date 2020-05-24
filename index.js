bfbdd5ec8bf8e4fce4e61110fab10b9407cf0783
ajamcato <ajamcato87@gmail.com>
2020-05-23 23:37:52 -0400

    added countdown functionality

--- index.js
+++ index.js
@@ -1,18 +1,19 @@
 let playing = false;
 let score;
 let action;
-let timeremaining;
+let timeremaining
+let correctAnswer;
 
 // if we click on start/reset button
-  document.getElementById('startreset').onclick =
-  function () {
-  //if we are playing
-  if(playing == true) {
-    location.reload(); //reload page
-  } else { //if we are not playing, set score to 0
-    //change mode to playing
+document.getElementById('startreset').onclick =
+  function() {
+    //if we are playing
+    if (playing == true) {
+      location.reload(); //reload page
+    } else { //if we are not playing, set score to 0
+      //change mode to playing
       playing = true;
-    //set score to 0
+      //set score to 0
       score = 0;
 
       document.getElementById('scorevalue').innerHTML = score;
@@ -21,9 +22,9 @@ let timeremaining;
 
       //show countdown box
       show('timeremaining');
-        timeremaining = 60;
+      timeremaining = 60;
 
-        document.getElementById('timeremainingvalue').innerHTML = timeremaining;
+      document.getElementById('timeremainingvalue').innerHTML = timeremaining;
 
       //change button to reset
       document.getElementById('startreset').innerHTML = 'Reset Game';
@@ -37,63 +38,146 @@ let timeremaining;
     }
 
 
+  }
+
+// clicking on an answer box
+
+document.getElementById('box1').onclick =
+function () {
+  //check is we are playing
+  if (playing == true) {//yes
+    if (this.innerHTML == correctAnswer) {
+      //correct answer
+
+      //increase score by 1
+      score++;
+
+      document.getElementById('scorevalue').innerHTML = score;
+
+          //hide wrong box and show correct box
+
+          hide('wrong');
+          show('correct');
+          setTimeout(function(){
+            hide('correct');
+          }, 1000);
+
+          //Generate new QA
+          generateQA();
+    } else {
+      //wrong answer
+      hide('correct');
+      show('wrong');
+      setTimeout(function () {
+        hide('wrong');
+      }, 1000);
+
+    }
+  }
 }
 
-          //yes -> continue
-          //no>-> gameover
-      // change button to Reset
-      // generate new Q&A
+for (i=1; i<5; i++) {
+  document.getElementById('box' + i).onclick =
+  function () {
+    //check is we are playing
+    if (playing == true) {//yes
+      if (this.innerHTML == correctAnswer) {
+        //correct answer
 
-// if we click on answer box
-  //if we are playing
-    //correct?
-      //yes
-        //increase score
-        //show correct box for 1sec
-        //generate Q&A
-      //if answer is wrong, show try again box
+        //increase score by 1
+        score++;
 
-      //functions
+        document.getElementById('scorevalue').innerHTML = score;
 
-      //start counter
-      function startCountdown () {
-        action = setInterval (function(){
-          timeremaining -= 1;
+            //hide wrong box and show correct box
 
-          document.getElementById('timeremainingvalue').innerHTML = timeremaining;
-          if(timeremaining == 0) { // game over
-            stopCountdown();
-            show('gameover');
-
-            document.getElementById('gameover').innerHTML =
-            "<p>Game Over!</p><p>Your score is " + score + ". </p>"
-
-              hide('timeremaining');
+            hide('wrong');
+            show('correct');
+            setTimeout(function(){
               hide('correct');
-              hide('wrong');
-              playing = false;
+            }, 1000);
 
-          }
+            //Generate new QA
+            generateQA();
+      } else {
+        //wrong answer
+        hide('correct');
+        show('wrong');
+        setTimeout(function () {
+          hide('wrong');
         }, 1000);
-      }
-
-      //stop counter
-
-      function stopCountdown () {
-        clearInterval(action);
-      }
-
-      //hide element
-      function hide (Id) {
-        document.getElementById(Id).style.display = 'none';
-      }
-
-      // show an element
-      function show (Id) {
-        document.getElementById(Id).style.display = 'block';
-      }
-
-      //generate question and multiple answers
-      function generateQA () {
 
       }
+    }
+  }
+}
+
+//functions
+
+//start counter
+function startCountdown() {
+  action = setInterval(function() {
+    timeremaining -= 1;
+
+    document.getElementById('timeremainingvalue').innerHTML = timeremaining;
+    if (timeremaining == 0) { // game over
+      stopCountdown();
+      show('gameover');
+
+      document.getElementById('gameover').innerHTML =
+        "<p>Game Over!</p><p>Your score is " + score + ". </p>"
+
+      hide('timeremaining');
+      hide('correct');
+      hide('wrong');
+      playing = false;
+
+    }
+  }, 1000);
+}
+
+//stop counter
+
+function stopCountdown() {
+  clearInterval(action);
+}
+
+//hide element
+function hide(Id) {
+  document.getElementById(Id).style.display = 'none';
+}
+
+// show an element
+function show(Id) {
+  document.getElementById(Id).style.display = 'block';
+}
+
+//generate question and multiple answers
+function generateQA() {
+  let x = 1 + Math.round(9 * Math.random());
+  let y = 1 + Math.round(9 * Math.random());
+  correctAnswer = x * y;
+
+  document.getElementById('question').innerHTML = x + 'x' + y;
+  let correctPosition = 1 + Math.round(3 * Math.random());
+  document.getElementById('box' + correctPosition).innerHTML = correctAnswer; //fill one box with correct answer
+
+  //fill other boxes with wrong answers
+
+  let answers = [correctAnswer];
+
+  for (i = 1; i < 5; i++) {
+    if (i !== correctPosition) {
+      let wrongAnswer;
+      do {
+
+          wrongAnswer = (1 + Math.round(9 * Math.random())) *
+            (1 + Math.round(9 * Math.random())); // a wrong answer
+        } while (answers.indexOf(wrongAnswer)>-1)
+
+        document.getElementById('box' + i).innerHTML = wrongAnswer;
+          answers.push(wrongAnswer);
+      }
+
+    }
+  }
